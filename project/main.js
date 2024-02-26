399811db9ef7d857ec9e4e814578434be0bb6040
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-02-26 15:09:41 -0500

    functionality in progress

--- project/main.js
+++ project/main.js
@@ -1,26 +1,73 @@
-const { app, BrowserWindow } = require("electron");
-// uses Chrmium
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
-//  check for OS
+
 const isMac = process.platform === "darwin";
-// dev
 const isDev = process.env.NODE_ENV !== "production";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resize",
-    width: isDev ? "1000" : "500",
+    width: isDev ? 1000 : 500,
     height: 600,
   });
 
-  // open dev tool if in dev mdoe
   if (isDev) {
+    // Open DevTools if in development mode
+    mainWindow.webContents.openDevTools();
   }
+
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+// create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About",
+    width: 300,
+    height: 600,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  const menuTemplate = [
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              {
+                label: "About",
+                click: createAboutWindow,
+              },
+            ],
+          },
+        ]
+      : []),
+    {
+      role: "fileMenu",
+    },
+    // windows menu
+    ...(!isMac
+      ? [
+          {
+            label: "Help",
+            submenu: [
+              {
+                label: "About",
+                click: createAboutWindow,
+              },
+            ],
+          },
+        ]
+      : []),
+  ];
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {

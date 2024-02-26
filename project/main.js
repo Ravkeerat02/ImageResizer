cc5595861ff64c093872900fa2cf7acbac4e38ed
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2024-02-26 14:49:16 -0500

    halfway

--- project/main.js
+++ project/main.js
@@ -1,16 +1,36 @@
 const { app, BrowserWindow } = require("electron");
 // uses Chrmium
 const path = require("path");
+//  check for OS
+const isMac = process.platform === "darwin";
+// dev
+const isDev = process.env.NODE_ENV !== "production";
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
-    titile: "Image Resize",
-    width: 500,
+    title: "Image Resize",
+    width: isDev ? "1000" : "500",
     height: 600,
   });
+
+  // open dev tool if in dev mdoe
+  if (isDev) {
+  }
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
